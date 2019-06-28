57399e96fa49d2a139bd1e005bd5069e237d1b88
Mari Wronka <marielle.wronka@hotmail.com>
2019-06-27 23:15:32 -0300

    feat(login)

---
+++ app-tem-convenio-cwi/Extensions/DesignableView.swift
@@ -0,0 +1,19 @@
+//
+//  DesignableView.swift
+//  app-tem-convenio-cwi
+//
+//  Created by Marielle Wronka on 27/06/2019.
+//  Copyright © 2019 Cwi Software. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class DesignableTextFied: UITextField {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        self.setLeftPaddingPoints(15)
+        self.setRightPaddingPoints(15)
+    }
+}
---
+++ app-tem-convenio-cwi/Extensions/UITextFiedExtension.swift
@@ -0,0 +1,68 @@
+//
+//  UIViewExtension.swift
+//  app-tem-convenio-cwi
+//
+//  Created by Marielle Wronka on 27/06/2019.
+//  Copyright © 2019 Cwi Software. All rights reserved.
+//
+
+import UIKit
+
+extension UITextField{
+    @IBInspectable
+    var cornerRadius: CGFloat {
+        get {
+            return layer.cornerRadius
+        }
+        set {
+            layer.cornerRadius = newValue
+            layer.masksToBounds = newValue > 0
+        }
+    }
+    
+    @IBInspectable
+    var borderWidth: CGFloat {
+        get {
+            return layer.borderWidth
+        }
+        set {
+            layer.borderWidth = newValue
+        }
+    }
+    
+    @IBInspectable
+    var borderColor: UIColor? {
+        get {
+            let color = UIColor.init(cgColor: layer.borderColor!)
+            return color
+        }
+        set {
+            layer.borderColor = newValue?.cgColor
+        }
+    }
+    
+    @IBInspectable
+    var shadowRadius: CGFloat {
+        get {
+            return layer.shadowRadius
+        }
+        set {
+            layer.shadowColor = UIColor.black.cgColor
+            layer.shadowOffset = CGSize(width: 0, height: 2)
+            layer.shadowOpacity = 0.4
+            layer.shadowRadius = shadowRadius
+        }
+    }
+    
+    func setLeftPaddingPoints(_ amount:CGFloat){
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: amount, height: self.frame.size.height))
+        self.leftView = paddingView
+        self.leftViewMode = .always
+    }
+    
+    func setRightPaddingPoints(_ amount:CGFloat) {
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: amount, height: self.frame.size.height))
+        self.rightView = paddingView
+        self.rightViewMode = .always
+    }
+}

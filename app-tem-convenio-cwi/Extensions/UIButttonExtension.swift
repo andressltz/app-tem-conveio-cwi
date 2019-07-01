118be9f878436308b2c2e38ee7e07e3a76662264
Mari Wronka <marielle.wronka@hotmail.com>
2019-07-01 20:32:13 -0300

    feat(views)

--- app-tem-convenio-cwi/Extensions/UIButttonExtension.swift
+++ app-tem-convenio-cwi/Extensions/UIButttonExtension.swift
@@ -8,32 +8,4 @@
 
 import UIKit
 
-extension UIButton {
-    @IBInspectable var cornerRadius: CGFloat {
-        get {
-            return layer.cornerRadius
-        }
-        set {
-            layer.cornerRadius = newValue
-            layer.masksToBounds = newValue > 0
-        }
-    }
-    
-    @IBInspectable var borderWidth: CGFloat {
-        get {
-            return layer.borderWidth
-        }
-        set {
-            layer.borderWidth = newValue
-        }
-    }
-    
-    @IBInspectable var borderColor: UIColor? {
-        get {
-            return UIColor(cgColor: layer.borderColor!)
-        }
-        set {
-            layer.borderColor = newValue?.cgColor
-        }
-    }
-}
+extension UIButton { }
--- app-tem-convenio-cwi/Extensions/UITextFiedExtension.swift
+++ app-tem-convenio-cwi/Extensions/UITextFiedExtension.swift
@@ -10,38 +10,6 @@ import UIKit
 
 extension UITextField{
     @IBInspectable
-    var cornerRadius: CGFloat {
-        get {
-            return layer.cornerRadius
-        }
-        set {
-            layer.cornerRadius = newValue
-            layer.masksToBounds = newValue > 0
-        }
-    }
-    
-    @IBInspectable
-    var borderWidth: CGFloat {
-        get {
-            return layer.borderWidth
-        }
-        set {
-            layer.borderWidth = newValue
-        }
-    }
-    
-    @IBInspectable
-    var borderColor: UIColor? {
-        get {
-            let color = UIColor.init(cgColor: layer.borderColor!)
-            return color
-        }
-        set {
-            layer.borderColor = newValue?.cgColor
-        }
-    }
-    
-    @IBInspectable
     var shadowRadius: CGFloat {
         get {
             return layer.shadowRadius

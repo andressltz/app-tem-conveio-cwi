88ec7d49a0334e8888ac2ddee9805b04df477121
Mari Wronka <marielle.wronka@hotmail.com>
2019-07-01 19:50:02 -0300

    register

--- app-tem-convenio-cwi/UI/Login/RegisterViewController.swift
+++ app-tem-convenio-cwi/UI/Login/RegisterViewController.swift
@@ -11,7 +11,11 @@ import UIKit
 class RegisterViewController: UIViewController {
   
     var clickViewPassword = false
+    
+    @IBOutlet weak var nameTextField: DesignableTextField!
+    @IBOutlet weak var emailTextField: DesignableTextField!
     @IBOutlet weak var passwordTextField: DesignablePasswordTextField!
+    
     @IBOutlet weak var passwordViewButton: UIButton!
     
     @IBAction func clickViewPassword(_ sender: UIButton) {
@@ -30,7 +34,27 @@ class RegisterViewController: UIViewController {
     }
     
     @IBAction func registerTouchUpInside(_ sender: UIButton) {
-        self.performSegue(withIdentifier: "MainSegue", sender: nil)
-    }
+        
+        let name: String
+        name = nameTextField.text!
+        
+        let email: String
+        email = emailTextField.text!
+        
+        let password: String
+        password = passwordTextField.text!
+        
+        if email.isEmpty || password.isEmpty || name.isEmpty {
+            let alert = UIAlertController(title: "Oops!", message: "É necessário preencher todos os campos.", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Default action"), style: .default))
+            self.present(alert, animated: true, completion: nil)
+        } else if !email.contains("@cwi.com.br") {
+            let alert = UIAlertController(title: "E-mail Inválido!", message: "O e-mail precisa ser do domínio da CWI.", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Default action"), style: .default))
+            self.present(alert, animated: true, completion: nil)
+        } else {
+            // TODO: Conectar com Firebase
+            self.performSegue(withIdentifier: "MainSegue", sender: nil)
+        }    }
     
 }

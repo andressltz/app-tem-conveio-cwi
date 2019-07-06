08e4a75a2efe48ef3988d22dfd070ceb1f43e0a9
Ândres Schultz <andreswinck@gmail.com>
2019-07-06 00:19:23 -0300

    feat(*) recommendation

--- app-tem-convenio-cwi/EstablishmentRecommendation/EstablishmentRecommendationViewController.swift
+++ app-tem-convenio-cwi/EstablishmentRecommendation/EstablishmentRecommendationViewController.swift
@@ -8,10 +8,67 @@
 
 import UIKit
 
-class EstablishmentRecommendationViewController: UIViewController {
+class EstablishmentRecommendationViewController: BaseImagePickerViewController {
+    
+    private let establishment: Establishment? = nil
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var establishmentImageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.addKeyboardObservers()
+//        self.presenter.view = self
+//        self.baseImagePickerCallback = self
+        self.addGestureToImageView(imageView: self.establishmentImageView)
+    }
     
     @IBAction func backgroundClick(_ sender: Any) {
         dismiss(animated: true)
     }
+    
+    @IBAction func recommendClickButton(_ sender: Any) {
+        // TODO: fazer
+        
+//        self.presenter.saveUser(withUID: (Session.instance.loggedUser?.uid)!,
+//                                withName: usernameTextField.text,
+//                                withEmail: emailTextField.text,
+//                                withProfession: professionTextField.text,
+//                                withBirth: birthTextField.text?.toDate(),
+//                                withPhone: phoneTextField.text,
+//                                withAbout: aboutTextView.text,
+//                                withImageURL: "")
+        
+        dismiss(animated: true)
+    }
+}
+
+extension EstablishmentRecommendationViewController { //}: ProfileEditViewType {
+    
+    func onImageSelected(image: UIImage) {
+        self.establishmentImageView.image = image
+    }
+    
+//    func onProfileSaved() {
+//        DispatchQueue.main.async {
+//            self.dismiss(animated: true, completion: nil)
+//        }
+//    }
+    
+//    func onFailure(error: BaseError) {
+//        self.showFailureAlert(withError: error)
+//    }
+}
+
+extension EstablishmentRecommendationViewController: BaseImagePickerProtocol {
+    
+    func onFailure() {
+        self.showFailureAlert(withError: ImagePickerError.failure)
+    }
+    
+    func onImageLoaded(image: UIImage) {
+//        self.presenter.image = image
+    }
+    
     
 }
